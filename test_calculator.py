9c234af0f8bf140a8b4a66a4517facefea43a2df
MaxMexico <maxencebrunet06@gmail.com>
2024-04-12 15:27:34 +0200

    update the letters

--- test_calculator.py
+++ test_calculator.py
@@ -1,16 +1,16 @@
-from calculator import addition
-from calculator import soustraction
-from calculator import division
+from calculator import Addition
+from calculator import Soustraction
+from calculator import Division
 
 
 def test_addition():
-    calc = addition(1, 1)
+    calc = Addition(1, 1)
     assert calc.addition() == 2
 
 def test_soustraction():
-    calc = soustraction(1, 1)
+    calc = Soustraction(1, 1)
     assert calc.soustraction() == 0
 
 def test_division():
-    calc = division(4, 2)
+    calc = Division(4, 2)
     assert calc.division() == 2
