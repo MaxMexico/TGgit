3d473fdcfc4b9ebd792933059a15cecd833dfe32
MaxMexico <maxencebrunet06@gmail.com>
2024-04-12 15:41:44 +0200

    upadate

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,17 @@
+"""test"""
 from calculator import Addition, Soustraction, Division
 
 def test_addition():
+    """test"""
     calc = Addition(1, 1)
     assert calc.calculate() == 2
 
 def test_soustraction():
+    """test"""
     calc = Soustraction(1, 1)
     assert calc.calculate() == 0
 
 def test_division():
+    """test"""
     calc = Division(4, 2)
     assert calc.calculate() == 2
