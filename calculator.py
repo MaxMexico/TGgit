d64c8136e420f219156969b8845bcaf2588dedf4
MaxMexico <maxencebrunet06@gmail.com>
2024-04-12 15:09:22 +0200

    add class instead of function

--- calculator.py
+++ calculator.py
@@ -1,12 +1,39 @@
-def addition(a,b):
-    result = a+b
-    return result
+class addition:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
 
-def soustraction(a,b):
-    result=a-b
-    return result
+    def addition(self):
+        return self.a + self.b
+    
 
-def division(a,b):
-    result=a/b
-    return result
+class soustraction:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def soustraction(self):
+        return self.a - self.b
+
+class division:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def division(self):
+        return self.a / self.b
+    
+
+
+# def addition(a,b):
+#     result = a+b
+#     return result
+
+# def soustraction(a,b):
+#     result=a-b
+#     return result
+
+# def division(a,b):
+#     result=a/b
+#     return result
     
